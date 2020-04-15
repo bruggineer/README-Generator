919f896daea397f67495aecb2d56c32027c89a33
TBruggeman <tsbruggeman@gmail.com>
2020-04-15 13:46:00 -0700

    synching github remote and local

--- readme-gen.js
+++ readme-gen.js
@@ -1,12 +1,13 @@
 //get the needed modules
+const fs = require("fs");
+cont inquirer = require("inquirer");
 
+const newReadMe = 'readme-file.json'
 
-
-
-
-
+//const writeFileAsync = util.promisify(fs.writeFile);
 
 //put the questions before the user and get the answers
+
 
 
 
@@ -14,3 +15,23 @@
 
 
 //call the functions and error trap
+// run async function to write file
+const writeFileAsync = (newData) => {
+    // convert the  JavaScript values to a JSON string
+    const stringifiedData = JSON.stringify(newData);
+  
+    // run async function to write file
+    fs.writeFile(FILE_NAME, stringifiedData, (error) => {
+      if (error) {
+        // if there is an error, print it
+        console.log('Async Write: NOT successful!');
+        console.log(error);
+      } else {
+        console.log('Async Write: successful!');
+        console.log(stringifiedData);
+      }
+    });
+  };
+
+// run the function
+writeFileAsync(NEW_DATA);
