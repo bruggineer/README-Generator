2b1590fb68d067f42465f85e1697b7fe1cdbc76d
TBruggeman <tsbruggeman@gmail.com>
2020-04-16 00:41:31 -0700

    for code review

--- api.js
+++ api.js
@@ -3,31 +3,27 @@ const fs = require("fs");
 const axios = require("./node_modules/axios");
 const inquirer = require("inquirer");
 
-getUser();
-async function getUser() {
-  try {
-    const { username } = await inquirer.prompt({
-      message: "Enter GitHub username:",
-      name: "username"
-    });
+function getUser() {
+  inquirer.prompt([
+    {
+    message: "Enter GitHub username:",
+    name: "username"
+    }
+  ]).then(answer => {
+  });
+  
 
-    const token = "a8faa74dfa7fae3c5f507aaafcbf06511f8d7b51"
+  const token = "e04a3f39f06735c4d8d21cef27a15c6fc9962240"
 
-    const { data } = await axios.get(
-      `https://api.github.com/users/${username}?access_token=${token}`
-    );
+  axios.get(
+    `https://api.github.com/users/${username}?access_token=${token}`
+  );
 
-    console.log(data);
-    console.log(data.email);
-    console.log(data.avatar_url);
-
-  } catch (err) {
-    console.log(err);
-  }
-
+  console.log(data);
+  console.log(data.email);
+  console.log(data.avatar_url);
 }
 
-  module.exports = {
-    githubEmail: data.email,
-    githubAvatar: data.avatar_url
-  }
+module.exports = {
+  username: username
+}
