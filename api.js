0b5e512a5f8a40c86d35f8db8162b94df1766ff4
TBruggeman <tsbruggeman@gmail.com>
2020-04-15 23:25:56 -0700

    inital coding for api.js, generateMarkdown.js, and index.js

--- api.js
+++ api.js
@@ -1,7 +1,33 @@
-const api = {
-  getUser(username) {
+//modules required
+const fs = require("fs");
+const axios = require("./node_modules/axios");
+const inquirer = require("inquirer");
 
+getUser();
+async function getUser() {
+  try {
+    const { username } = await inquirer.prompt({
+      message: "Enter GitHub username:",
+      name: "username"
+    });
+
+    const token = "a8faa74dfa7fae3c5f507aaafcbf06511f8d7b51"
+
+    const { data } = await axios.get(
+      `https://api.github.com/users/${username}?access_token=${token}`
+    );
+
+    console.log(data);
+    console.log(data.email);
+    console.log(data.avatar_url);
+
+  } catch (err) {
+    console.log(err);
   }
-};
 
-module.exports = api;
+}
+
+  module.exports = {
+    githubEmail: data.email,
+    githubAvatar: data.avatar_url
+  }
--- index.js
+++ index.js
@@ -1,9 +1,55 @@
+//get the needed modules
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 const questions = [
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Enter the Project Title:"
+        },
+        {
+            type: "input",
+            name: "projectDesc",
+            message: "Enter Description:"
+        },
+        {
+            type: "input",
+            name: "install",
+            message: "Enter directions for installation:"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Enter usage:"
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "Enter license:"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Enter contributors:"
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "Enter tests:"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "Where can questions be submitted?:"
+        },
+    ]),
+]
 
-];
-
-function writeToFile(fileName, data) {
-}
 
 function init() {
 
