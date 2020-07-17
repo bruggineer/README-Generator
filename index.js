a9f5a76d41209b82bec1a990ae06a5054f4a08c2
TBruggeman <tsbruggeman@gmail.com>
2020-07-16 22:19:24 -0700

    functional readme CLI with api key removed

--- index.js
+++ index.js
@@ -1,65 +1,96 @@
-//get the needed modules
+//calls node dependencies and app files
 const fs = require("fs");
 const inquirer = require("inquirer");
-const util = require("util");
-const api = require("./api");
+const axios = require("./node_modules/axios");
 const generateMarkdown = require("./generateMarkdown");
 
-async function askQuestions() {
-    const answers = await inquirer.prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "Enter the Project Title:"
-        },
-        {
-            type: "input",
-            name: "projectDesc",
-            message: "Enter Description:"
-        },
-        {
-            type: "input",
-            name: "install",
-            message: "Enter directions for installation:"
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "Enter usage:"
-        },
-        {
-            type: "input",
-            name: "license",
-            message: "Enter license:"
-        },
-        {
-            type: "input",
-            name: "contributing",
-            message: "Enter contributors:"
-        },
-        {
-            type: "input",
-            name: "test",
-            message: "Enter tests:"
-        },
-        {
-            type: "input",
-            name: "questions",
-            message: "Where can questions be submitted?:"
-        },
-    ])
-}
+//command line input prompts
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the Project Title:"
+  },
+  {
+    type: "input",
+    name: "projectDesc",
+    message: "Enter Description:"
+  },
+  {
+    type: "input",
+    name: "repo",
+    message: "Enter GitHub repo name"
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "Enter directions for installation:"
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "Enter license:"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter contributors:"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Enter tests:"
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "Where can questions be submitted?:"
+  },
+]
 
-module.exports = {
-    askQuestions: askQuestions
-}
+async function getUser() {
 
-async function init() {
-    const githubData = await api.getUser();
+  const API_KEY = "key goes here"
+
+  const replies = await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "username",
+        message: "Enter GitHub username:",
+      },
+    ]);
+
+  return await axios.get(
+    `https://api.github.com/users/${replies.username}?access_token=${API_KEY}`
+  ).then(function (data) {
+    console.log(data.data);
+    const email = data.data.email;
+    const avatar = data.data.avatar_url;
+    console.log(email)
+    console.log(avatar)
+    const githubData = {
+      email: email,
+      avatar: avatar,
+      username: data.data.login
+    }
     console.log(githubData);
-    await askQuestions();
-    generateMarkdown.generateMarkdown(answers, api.githubData);
-}
+    return githubData;
+  })
+};
 
+//call api for GitHub user data, ask the questions and collect answers
+async function init() {
+  const githubData = await getUser()
+  await inquirer.prompt(questions).then(function (answers) {
+    console.log(answers)
+    generateMarkdown(answers, githubData)
+  })
+
+};
+
+//start the app
 init();
+
+
 
